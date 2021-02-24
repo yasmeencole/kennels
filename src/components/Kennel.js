52c5115d3d8f96e7fa4ba679c55d04e6feb260f9
yasmeencole <yasmeencole1@mail.com>
2021-02-24 15:33:32 -0600

    made styling updates to Kennel.css and Animal.css, made updates to AnimalCard.js and Kennel.js

--- src/components/Kennel.js
+++ src/components/Kennel.js
@@ -1,5 +1,6 @@
 import React from "react"
 import "./Kennel.css"
+import { AnimalCard } from "./animal/AnimalCard"
 
 export const Kennel = () => { 
     const kennel = {
@@ -7,7 +8,7 @@ export const Kennel = () => {
         locations: [
             {
                 name: "Nashville North",
-                address: "500 Puppy Way"
+                address: "500 Puppy Way, Nashville, TN 37210"
             }
         ]
     }
@@ -21,6 +22,12 @@ export const Kennel = () => {
             <div>Visit Us at the {kennel.locations[0].name} Location</div>
             <div>{kennel.locations[0].address}</div>
         </address>
-    </>
+        <h2>Animals</h2>
+        <article className="animals">
+            <AnimalCard />
+            <AnimalCard />
+            <AnimalCard />
+        </article>
+    </>    
     )
 }
